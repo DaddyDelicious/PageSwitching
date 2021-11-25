01f75ab7f50d8e06111f67e212de8a45240af617
Klas Nordquist <78430350+DaddyDelicious@users.noreply.github.com>
2021-11-25 11:12:52 +0100

    Add files via upload
    Bara för att testa sidbyte via knappar.

---
+++ src/Components/FrontPgButtons.jsx
@@ -0,0 +1,46 @@
+import React, { Component } from "react";
+import "../frontPgButtons.css";
+import Hjärta from "../Assets/Hjärta.png";
+import StoreShape from "../Assets/StoreShape.png";
+import Ica from "../Assets/Ica.png";
+import Coop from "../Assets/Coop.png";
+import ICAText from "../Assets/ICAText.png";
+import CoopText from "../Assets/CoopText.png";
+
+//className="buttonLook"
+class FrontPageButtons extends React.Component {
+  render() {
+    console.log("FrontPageButton - Clicked");
+    return (
+      <div className="Screen " role="group">
+        <button
+          onClick={() => console.log("FrontPageButton - Clicked")}
+          className="StoreButton"
+        >
+          <img src={Ica} alt="ica" />
+
+          <img src={ICAText} alt="icatext" />
+        </button>
+
+        <button
+          onClick={() => console.log("FrontPageButton - Clicked")}
+          className="StoreButton"
+        >
+          <img src={Coop} alt="coop" />
+
+          <img src={CoopText} alt="CoopText" />
+        </button>
+
+        <button
+          onClick={() => console.log("FrontPageButton - Clicked")}
+          className="StoreButton"
+        >
+          <img src={Ica} alt="ica" />
+          <img src={ICAText} alt="icatext" />
+        </button>
+      </div>
+    );
+  }
+}
+
+export default FrontPageButtons;
